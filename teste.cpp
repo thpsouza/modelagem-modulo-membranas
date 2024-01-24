68ea022a936f18615aa82e2e75258b170888de91
Thiago Souza <thiagopsouza23@gmail.com>
2024-01-24 11:22:14 -0300

    teste

---
+++ teste.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    // Dicionário de dados
+
+    // Corpo
+    cout << "teste" << endl;
+
+    return 0;
+}
